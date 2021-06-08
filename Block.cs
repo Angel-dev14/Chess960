45cc348f3b6cbcbdbc3e2e888d6c650d386c9fcf
Saveski Hristijan <saveskihristijan@yahoo.com>
2021-06-08 23:06:23 +0200

    Block i Figure class

---
+++ Block.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Block : Button
+{
+    public Color Color { get; set; }
+    public Figure Figure { get; set; }
+    public bool Disabled { get; set; }
+    public Block() : base()
+    {
+        Figure = new Figure();
+    }
+    public void AddFigure(Figure figure)
+    {
+        this.Figure = figure;
+
+        ChangeBackgroundImage();
+    }
+    public void ClearFigure()
+    {
+        this.BackgroundImage = null; // neka si sedi slikata vo slucaj da ne treba da se smeni, samo slikata na kopceto (blokot) se menja
+    }
+    private void ChangeBackgroundImage()
+    {
+        this.BackgroundImage = Figure.Icon;
+    }
+    public void Available()
+    {
+        this.BackColor = Color.FromArgb(252, 249, 120); // light yellow
+    }
+}
---
+++ Figure.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public class Figure
+{
+    public int Type { get; set; }
+    public Image Icon { get; set; } // contains the image of the figure
+}
