a61a5970ea7e3ba88e7eb5354d94d32452d5a9ea
Saveski Hristijan <hristijan.saveski@students.finki.ukim.mk>
2021-06-08 17:45:48 +0200

    This is my first change

---
+++ Test1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess960
+{
+    class Test1
+    {
+        //se glea comment?
+        int a = 10;
+    }
+}
